f38717672057c348276c8958cde0733e523f731b
solomonilori <solomonilori@gmail.com>
2023-04-18 16:45:07 +0100

    load products added

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from '../models/product';
 
 @Component({
   selector: 'app-products',
@@ -6,10 +7,62 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./products.component.scss']
 })
 export class ProductsComponent implements OnInit {
+  theProducts: Product[] = [];
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  getTextDecoration(outOfStock = false){
+    if (outOfStock) {
+      return 'line-through';
+    }else{
+      return 'none';
+    }
+  }
+
+  
+  loadProducts(){
+    const products: Product[] = [
+      {
+      id: 1,
+      productName: 'white shirt',
+      amount: 1000,
+      category: 'clothes',
+      outOfStock: false,
+    },
+      {
+      id: 1,
+      productName: 'blue shirt',
+      amount: 1500,
+      category: 'clothes',
+      outOfStock: false,
+    },
+      {
+      id: 1,
+      productName: 'Agbada',
+      amount: 3000,
+      category: 'clothes',
+      outOfStock: true,
+    },
+      {
+      id: 1,
+      productName: 'white shoe',
+      amount: 2500,
+      category: 'shoes',
+      outOfStock: true,
+    },
+      {
+      id: 1,
+      productName: 'palm sandal',
+      amount: 1000,
+      category: 'shoes',
+      outOfStock: false,
+    },
+  ];
+  
+    this.theProducts = products;
   }
 
 }
